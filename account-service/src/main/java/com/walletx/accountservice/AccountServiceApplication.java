97df4d78558017dd66ad3a4fe4d8aefd1a2e649b
DcTerrance88 <condefroilan@gmail.com>
2026-04-13 15:49:04 -0500

    feat(account-service): initialize account service module
    - Add account-service Maven module with parent inheritance
    - Add pom.xml with required dependencies including common module
    - Add account-service.yml to config server on port 8082
    - Uncomment account-service module in parent pom.xml

---
+++ account-service/src/main/java/com/walletx/accountservice/AccountServiceApplication.java
@@ -0,0 +1,18 @@
+package com.walletx.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.walletx.accountservice",
+        "com.walletx.common"
+})
+public class AccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
+
+}
